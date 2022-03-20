a5fa33b6f405ffec4ded088aec50567b1e010c73
TheGrumpyGameDev <ernestpazera@msn.com>
2022-03-20 05:38:22 -0500

    added credits

--- TGGD_ugj61/TGGD_ugj61/Program.vb
+++ TGGD_ugj61/TGGD_ugj61/Program.vb
@@ -12,22 +12,33 @@ Module Program
     End Sub
     Private Const EmbarkText = "Embark!"
     Private Const QuitText = "Quit"
+    Private Const CreditsText = "Credits"
     Private Sub MainMenu()
         Dim done = False
         While Not done
+            AnsiConsole.WriteLine()
             Dim prompt As New SelectionPrompt(Of String) With
             {
                 .Title = "[olive]Main Menu:[/]"
             }
             prompt.AddChoice(EmbarkText)
+            prompt.AddChoice(CreditsText)
             prompt.AddChoice(QuitText)
             Select Case AnsiConsole.Prompt(prompt)
                 Case QuitText
                     done = ConfirmQuit()
                 Case EmbarkText
                     Embark.Run()
+                Case CreditsText
+                    ShowCredits()
             End Select
         End While
+    End Sub
+
+    Private Sub ShowCredits()
+        AnsiConsole.WriteLine()
+        AnsiConsole.MarkupLine("Art Credits:")
+        AnsiConsole.MarkupLine("https://game-icons.net/1x1/delapouite/mustache.html")
     End Sub
 
     Private Function ConfirmQuit() As Boolean
