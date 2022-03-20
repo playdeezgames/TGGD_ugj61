996c4b6a743809abb2536de86391616501a5fd76
TheGrumpyGameDev <ernestpazera@msn.com>
2022-03-19 20:00:45 -0500

    beep

--- TGGD_ugj61/TGGD_ugj61/SfxHandler.vb
+++ TGGD_ugj61/TGGD_ugj61/SfxHandler.vb
@@ -2,6 +2,8 @@
 
 Module SfxHandler
     Sub PlaySfx(sfx As Sfx)
-
+#Disable Warning CA1416 ' Validate platform compatibility
+        Console.Beep(100, 100)
+#Enable Warning CA1416 ' Validate platform compatibility
     End Sub
 End Module
